73bf4fba224a23c43490e45faf7e1f76f38c0f37
SergioMontoro <84936651+SergioMontoro@users.noreply.github.com>
2023-12-18 17:35:30 +0000

    To do list with Feth

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,101 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	const [task, setTask] = useState(" ");
+	const [taskList, setList] = useState([]);
+	const [visible, setVisible] = useState("none");
+	const [hoverIndex, setHoverIndex] = useState(null);
+
+
+	function getTasks() {
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/smontoro15', {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			})
+			.then(resp => resp.json()
+			)
+			.then(data => {
+				setList(data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
+
+	function updateTasks(dinamicList) {
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/smontoro15', {
+			method: "PUT",
+			body: JSON.stringify(dinamicList),
+			headers: {
+				"Content-Type": "application/json"
+			},
+			})
+			.then(resp => resp.json()
+			)
+			.then(data => {})
+			.catch(error => {
+				console.log(error);
+			});
+	}
+
+	function taskInput(event) {
+		setTask({label: event.target.value, done: false});
+		console.log(task);
+	}
+
+	function addItem(event) {
+		if(event.keyCode === 13) {
+			const dinamicList = [...taskList, task];
+			setList(dinamicList);
+
+			event.target.value = "";
+
+			updateTasks(dinmicList);
+			
+			getTasks();
+		}		
+	}
+
+	function deleteItem(deletedItem) {
+		const modifiedTaskList = taskList.filter((item) => item !== deletedItem);
+		setList(modifiedTaskList);
+
+		updateTasks(modifiedTaskList);
+	}
+
+
+	useEffect(() => {
+		getTasks();
+	},[]);
+	
+
+	function showButton(index) {
+		setVisible("block");
+		setHoverIndex(index);
+	}
+
+	function hideButton() {
+		setVisible("none");
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex card w-25 mx-auto p-4 bg-dark">
+			<div className="card-title text-center">
+				<h1 className="text-white p-3">TO DO LIST</h1>
+			</div>
+			<input type="text" className="form-control" onChange={taskInput} onKeyDown={addItem} placeholder="Add your task here"/>
+			<ul className="list-group">
+				{taskList.map((item, index) => (
+         			 <li className="d-flex justify-content-between align-items-center list-group-item" key={index} onMouseEnter={() => showButton(index)} onMouseLeave={hideButton}>{item.label}<button className="btn btn-light" onClick={() => deleteItem(item)} style={{display: hoverIndex === index ? visible : "none"}}>X</button></li>
+       			))}
+			</ul>
+			<div className="text-white mt-4">
+				<span>{taskList.length} items left</span>
+			</div>
 		</div>
 	);
 };
